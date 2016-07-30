752eeaf16654ebf076b80a8e7b3051c699955f83
rdas6313 <rdas6313@gmail.com>
2016-07-30 19:45:12 +0530

    some link added to header file navbar

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -94,11 +94,11 @@
         </div>
         <div class="grid_12">
             <ul class="nav main">
-                <li class="ic-dashboard"><a href="index.html"><span>Dashboard</span></a> </li>
+                <li class="ic-dashboard"><a href="index.php"><span>Dashboard</span></a> </li>
                 <li class="ic-form-style"><a href=""><span>User Profile</span></a></li>
-				<li class="ic-typography"><a href="changepassword.html"><span>Change Password</span></a></li>
-				<li class="ic-grid-tables"><a href="inbox.html"><span>Inbox</span></a></li>
-                <li class="ic-charts"><a href="postlist.html"><span>Visit Website</span></a></li>
+				<li class="ic-typography"><a href="changepassword.php"><span>Change Password</span></a></li>
+				<li class="ic-grid-tables"><a href="inbox.php"><span>Inbox</span></a></li>
+                <li class="ic-charts"><a href="#"><span>Visit Website</span></a></li>
             </ul>
         </div>
         <div class="clear">
